054c95b475b04b59ac59100f366d90b86d9919e8
Ridwan Setio Budi <108109278+Ridwanstbd@users.noreply.github.com>
2024-11-07 07:59:52 +0700

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout({ children }) {
+    return (
+        <html>
+            <head>
+                <title>LEarn</title>
+            </head>
+            <body>{children}</body>
+        </html>
+    )
+}
